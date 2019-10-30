5bce8b8b7284ed29ba506defcfdaf1a5d85ca5cf
madasilva1 <madasilva195@gmail.com>
2019-10-30 15:49:47 -0400

    Score board has been add

--- app/src/main/java/com/example/android/courtcount/MainActivity.java
+++ app/src/main/java/com/example/android/courtcount/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.courtcount;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
 }
